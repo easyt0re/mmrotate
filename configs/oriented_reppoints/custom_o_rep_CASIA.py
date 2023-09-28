44d0bac31e70eef964793d9468c4bfa409ed34b7
easyt0re <harry.yangwang@gmail.com>
2023-09-29 00:04:50 +0200

    added configs for o rep in a hurry, added script run

---
+++ configs/oriented_reppoints/custom_o_rep_CASIA.py
@@ -0,0 +1,73 @@
+_base_ = 'oriented_reppoints_r50_fpn_1x_dota_le135.py'
+
+dataset_type = 'DOTADataset'
+classes = ('ship', 'submarine')
+n_classes = len(classes)
+
+class_weight = [1., 20., 0.5]
+
+# classes = ('ship', 'submarine')
+# data_root = 'datasets/split_data/'
+# data_root = 'datasets/CASIA-Ship/'
+# data_root = 'data/split_ss_dota/'
+# data_root = '../mmr/datasets/SRSDD_DOTA/'
+data_root = '../mmr/datasets/CASIA-Ship/'
+
+# n_frozen_stages = 1
+# n_frozen_epoch = 5
+
+data = dict(
+    samples_per_gpu=2,  # batch-size
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train/labels/',
+        img_prefix=data_root + 'train/images/'),
+
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'val/labels/',
+        img_prefix=data_root + 'val/images/'),
+
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/labels/',
+        img_prefix=data_root + 'test/images/'),
+)
+
+model = dict(
+    # roi_head=dict(
+        bbox_head=dict(
+            num_classes=n_classes,
+            # loss_cls=dict(class_weight=class_weight)
+            # changed this based on this page
+            # https://mmdetection.readthedocs.io/en/v2.9.0/_modules/mmdet/models/losses/cross_entropy_loss.html
+        )
+    # ),
+    # backbone=dict(frozen_stages=n_frozen_stages)
+)
+
+# custom_imports = dict(imports=['unfreeze_backbone_epoch_based_hook'], allow_failed_imports=False)
+# custom_hooks = [dict(type="UnfreezeBackboneEpochBasedHook", unfreeze_epoch=n_frozen_epoch)]
+
+# schedule
+optimizer = dict(lr=0.008) # default 0.008
+# learning policy
+lr_config = None
+# lr_config = dict(
+#     policy='step',
+#     warmup='linear',
+#     warmup_iters=500,
+#     warmup_ratio=1.0 / 3,
+#     step=[8, 11]
+#     )
+runner = dict(type='EpochBasedRunner', max_epochs=80)
+checkpoint_config = dict(interval=10)
+
+#default runtime
+# ckpt_name = 'epoch_1.pth' # specify ckpt file name here
+# resume_from = './work_dirs/resume_ckpt/' + ckpt_name
+workflow = [('train', 1)]
---
+++ train_models.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Train model 1
+echo "Training model 1..."
+python tools/train.py configs/oriented_reppoints/custom_o_rep_SRSDD.py
+# Check if model 1 was trained successfully
+if [ $? -eq 0 ]; then
+  echo "Model 1 trained successfully."
+else
+  echo "Model 1 failed to train."
+  exit 1 # Exit the script with an error code
+fi
+# Train model 2
+echo "Training model 2..."
+python tools/train.py configs/oriented_reppoints/custom_o_rep_CASIA.py
+# Check if model 2 was trained successfully
+if [ $? -eq 0 ]; then
+  echo "Model 2 trained successfully."
+else
+  echo "Model 2 failed to train."
+  exit 2 # Exit the script with a different error code
+fi
+# If both models were trained successfully, exit with a success code
+echo "Both models trained successfully."
+exit 0
