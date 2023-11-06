a6c177fd3d108d41d71f59ce7c0832397cdbc738
easyt0re <harry.yangwang@gmail.com>
2023-11-06 22:27:35 +0100

    expanded to train 3 networks

--- train_rfrcnn_both.sh
+++ train_rfrcnn_both.sh
@@ -9,6 +9,7 @@ else
   echo "Model 1 failed to train."
   exit 1 # Exit the script with an error code
 fi
+
 # Train model 2
 echo "Training model 2..."
 python tools/train.py configs/rotated_retinanet/t_custom_r_ret_CASIA.py
@@ -19,6 +20,18 @@ else
   echo "Model 2 failed to train."
   exit 2 # Exit the script with a different error code
 fi
+
+# Train model 3
+echo "Training model 3..."
+python tools/train.py configs/oriented_reppoints/t_custom_o_rep_CASIA.py
+# Check if model 3 was trained successfully
+if [ $? -eq 0 ]; then
+  echo "Model 3 trained successfully."
+else
+  echo "Model 3 failed to train."
+  exit 3 # Exit the script with a different error code
+fi
+
 # If both models were trained successfully, exit with a success code
-echo "Both models trained successfully."
+echo "Three models trained successfully."
 exit 0
