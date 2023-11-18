277f65a66d99a04b4ef4105f1dfa1796aa688d1b
easyt0re <harry.yangwang@gmail.com>
2023-11-18 01:04:16 +0100

    added all vanilla files and auto script

---
+++ configs/oriented_reppoints/vanilla_o_rep_CASIA.py
@@ -0,0 +1,42 @@
+_base_ = 'oriented_reppoints_r50_fpn_1x_dota_le135.py'
+
+dataset_type = 'DOTADataset'
+classes = ('ship', 'submarine')
+n_classes = len(classes)
+
+data_root = '../mmr/datasets/CASIA-Ship/'
+
+data = dict(
+    samples_per_gpu=2,  # batch-size
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train/labels/',
+        img_prefix=data_root + 'train/images/'),
+
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'val/labels/',
+        img_prefix=data_root + 'val/images/'),
+
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/labels/',
+        img_prefix=data_root + 'test/images/'),
+)
+
+# schedule
+optimizer = dict(lr=0.008) # default 0.008
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=5000,
+    warmup_ratio=1.0 / 3,
+    step=[100]
+    )
+runner = dict(type='EpochBasedRunner', max_epochs=50)
+checkpoint_config = dict(interval=10)
---
+++ configs/rotated_retinanet/vanilla_r_ret_SRSDD.py
@@ -0,0 +1,40 @@
+_base_ = 'rotated_retinanet_obb_r50_fpn_1x_dota_le90.py'
+
+dataset_type = 'DOTADataset'
+classes = ('ore-oil', 'Cell-Container', 'Fishing', 'LawEnforce', 'Dredger', 'Container')
+n_classes = len(classes)
+
+data_root = '../mmr/datasets/SRSDD_DOTA/'
+
+data = dict(
+    samples_per_gpu=2,  # batch-size
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'train/labels/',
+        img_prefix=data_root + 'train/images/'),
+
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'val/labels/',
+        img_prefix=data_root + 'val/images/'),
+
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'test/labels/',
+        img_prefix=data_root + 'test/images/'),
+)
+
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=5000,
+    warmup_ratio=1.0 / 3,
+    step=[100]
+    )
+runner = dict(type='EpochBasedRunner', max_epochs=50)
+checkpoint_config = dict(interval=10)
